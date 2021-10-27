df7bdffd93e1e4eb29736f5aaf1277153c214f2e
“kkjos” <k19951227@gmail.com>
2021-10-27 02:54:29 +0000

    [finish]chapter8

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -13,8 +13,11 @@ class BlogsController < ApplicationController
 
   def create
     @blog = Blog.new(blog_params)
-    @blog.save
-    redirect_to blog_path(blog.id)
+    if @blog.save
+      redirect_to blog_path(blog.id)
+    else
+      render :new
+    end
   end
 
   def edit
